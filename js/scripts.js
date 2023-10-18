48b8448cdd6abc5f4780000ba054723e98f8cc4f
Brianca Knight <briancaknight@gmail.com>
2023-10-18 10:30:02 -0700

    Add function to increase font size.
    Co-authored-by: Trent Dietzel <dietzeltrent@gmail.com> and Jonathan Song <jonathan91790@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,20 @@
+let fontSize = 12
 function getUserSub() {
     let form = document.querySelector("form");
     form.onsubmit = function(e) {
         e.preventDefault();
+        fontSize = fontSize + 5;
         const userSub = document.getElementById("userSub").value;
-        result = userSub.toUpperCase();
+        const result = userSub.toUpperCase();
         document.querySelector("span#result").innerText = result;
+        let sizestring = fontSize.toString();
+        document.querySelector("span#result").style.fontSize = sizestring + "px";
     }
 }
 
 window.onload = function(){
   getUserSub();
 }
+
+// sizestring.style.fontsize
+// document.getElementById("demo").style.fontsize = fontsize;
